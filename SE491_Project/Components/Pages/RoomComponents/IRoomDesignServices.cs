0bb2e88f61219406fb1c05519cfe593fddf84cd8
Emohamed <mhmoed8000900@gmail.com>
2025-05-14 14:01:11 +0200

    Add IRoomDesignServices interface for room management
    Introduced the `IRoomDesignServices` interface in the
    `SE491_Project.Components.Pages.RoomComponents` namespace.
    This interface provides methods for managing `Room` entities,
    including deletion, retrieval by ID, listing all rooms,
    saving, and updating rooms.

---
+++ SE491_Project/Components/Pages/RoomComponents/IRoomDesignServices.cs
@@ -0,0 +1,13 @@
+﻿using SE491_Project.Data.Entities;
+
+namespace SE491_Project.Components.Pages.RoomComponents
+{
+    public interface IRoomDesignServices
+    {
+        void Delete(Room room);
+        Room GetRoomByID(Guid id);
+        List<Room> GetRooms();
+        Room Save(Room room);
+        Room Update(Room room);
+    }
+}
